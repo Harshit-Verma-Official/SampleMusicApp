417930160062f951867dd77d62e5b7531aa34b19
Harshit Verma <harshitv9984@gmail.com>
2019-03-30 20:56:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/samplemusicapp/PlayerAdapter.java
@@ -0,0 +1,20 @@
+package com.example.samplemusicapp;
+
+public interface PlayerAdapter {
+
+    void loadMedia(int resourceId);
+
+    void release();
+
+    boolean isPlaying();
+
+    void play();
+
+    void reset();
+
+    void pause();
+
+    void initializeProgressCallback();
+
+    void seekTo(int position);
+}
